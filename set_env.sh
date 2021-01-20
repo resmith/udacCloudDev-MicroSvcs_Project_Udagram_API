5c1733f1efa9793433ab1e0c15c06641c4bbe0e7
Robert Smith <txresmith@gmail.com>
2021-01-19 21:19:20 -0600

    Initial commit. Included .travis.yml. Reworked code previous to seperating api vs front-end

---
+++ set_env.sh
@@ -0,0 +1,27 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+
+
+# *** AWS CLOUD INFO
+export POSTGRES_USERNAME=udagramapp
+export POSTGRES_PASSWORD=mysecretpassword
+export POSTGRES_HOST=udagramdb2.c3sjnm8grqvi.us-west-2.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=udacclouddevudagram-resmith-test
+export AWS_REGION=us-west-2
+export AWS_PROFILE=default
+export JWT_SECRET=notASecret
+export URL=http://localhost:8080
+
+
+# *** Local POSTGRES DB
+# export POSTGRES_USERNAME=udagramappuser
+# export POSTGRES_PASSWORD=mysecretpassword
+# export POSTGRES_HOST=localhost
+# export POSTGRES_DB=udagramdb
+# export AWS_BUCKET=udacclouddevudagram-resmith-test
+
+# export AWS_REGION=us-west-2
+# export AWS_PROFILE=default
+# export JWT_SECRET=notASecret
+# export URL=http://localhost:8080
